f93d3d37a26bcc3ca9cfdb7a67e5f0339a335851
Inga355 <albi1303@gmail.com>
2025-02-17 18:55:41 +0100

    added functionality to update route

--- app.py
+++ app.py
@@ -5,6 +5,13 @@ import json
 app = Flask(__name__)
 
 
+def fetch_post_by_id(post_id):
+    with open('data.json', 'r') as json_file:
+        blog_posts = json.load(json_file)
+        fetch_post = next((post for post in blog_posts if post.get('id') == post_id), None)
+        return fetch_post
+
+
 @app.route('/')
 def index():
     with open('data.json', 'r') as json_file:
@@ -52,8 +59,24 @@ def delete(post_id):
 def update(post_id):
     with open('data.json', 'r') as json_file:
         blog_posts = json.load(json_file)
+        post = next((post for post in blog_posts if post.get('id') == post_id), None)
+        if request.method == 'POST':
+            blog_posts = [post for post in blog_posts if post.get('id') != post_id]
+            new_author = request.form['author']
+            new_title = request.form['title']
+            new_content = request.form['content']
+            new_post = {
+                'id': post_id,
+                'author': new_author,
+                'title': new_title,
+                'content': new_content
+            }
+            blog_posts.append(new_post)
+            with open('data.json', 'w') as f:
+                json.dump(blog_posts, f, indent=4)
+            return redirect(url_for('index'))
+        return render_template('update.html', post=post)
 
-    return render_template('update.html', post=blog_posts)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)

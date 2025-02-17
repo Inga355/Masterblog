67213ee7694e7815865c727f44dc1d053a2b8303
Inga355 <albi1303@gmail.com>
2025-02-17 12:37:39 +0100

    added functionality to add a post

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for, redirect
 import json
 
+from werkzeug.utils import redirect
+
 app = Flask(__name__)
+
+
 
 
 @app.route('/')
@@ -13,10 +17,24 @@ def index():
 
 @app.route('/add', methods=['GET', 'POST'])
 def add():
-    if request.method == 'POST':
-        # We will fill this in the next step
-        pass
-    return render_template('add.html')
+    with open('data.json', 'r') as json_file:
+        blog_posts = json.load(json_file)
+        if request.method == 'POST':
+            print(f'erster Post : {blog_posts}')
+            new_author = request.form['author']
+            new_title = request.form['title']
+            new_content = request.form['content']
+            new_post = {
+                'author': new_author,
+                'title': new_title,
+                'content': new_content
+            }
+            blog_posts.append(new_post)
+            print(blog_posts)
+            with open('data.json', 'w') as f:
+                json.dump(blog_posts, f, indent=4)
+            return redirect(url_for('index'))
+        return render_template('add.html')
 
 
 if __name__ == '__main__':

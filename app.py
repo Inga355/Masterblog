170a56b49ae6cc6471bd39d5c18e4d46c36aa664
Inga355 <albi1303@gmail.com>
2025-02-17 11:17:32 +0100

    create index route with function to get data from JSON

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
-from flask import Flask
-
+from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_World():
-    return 'Hello World'
+def index():
+    with open('data.json', 'r') as json_file:
+        blog_posts = json.load(json_file)
+    return render_template('index.html', post=blog_posts)
 
 
 if __name__ == '__main__':

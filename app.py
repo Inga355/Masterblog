fb4554d76a223ef5014f0866ce49265322300b23
Inga355 <albi1303@gmail.com>
2025-02-17 11:33:26 +0100

    created add route and simple form for adding a new post

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import json
 
 app = Flask(__name__)
@@ -9,6 +9,14 @@ def index():
     with open('data.json', 'r') as json_file:
         blog_posts = json.load(json_file)
     return render_template('index.html', post=blog_posts)
+
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        # We will fill this in the next step
+        pass
+    return render_template('add.html')
 
 
 if __name__ == '__main__':

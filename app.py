cb4158b28a16b06723991939d26ea1e544a55a80
Inga355 <albi1303@gmail.com>
2025-02-17 14:06:08 +0100

    added auto id

--- app.py
+++ app.py
@@ -16,18 +16,21 @@ def index():
 def add():
     with open('data.json', 'r') as json_file:
         blog_posts = json.load(json_file)
+        highest_id = 0
+        for post in blog_posts:
+            if post['id'] > highest_id:
+                highest_id = post['id']
         if request.method == 'POST':
-            print(f'erster Post : {blog_posts}')
             new_author = request.form['author']
             new_title = request.form['title']
             new_content = request.form['content']
             new_post = {
+                'id': highest_id + 1,
                 'author': new_author,
                 'title': new_title,
                 'content': new_content
             }
             blog_posts.append(new_post)
-            print(blog_posts)
             with open('data.json', 'w') as f:
                 json.dump(blog_posts, f, indent=4)
             return redirect(url_for('index'))
